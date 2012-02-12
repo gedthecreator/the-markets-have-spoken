3e325f163413ea29ded03d6784ac2a0a7e3a2621
Ged <macdermott@gmail.com>
2012-02-12 08:29:18 +0000

    Added tests for update

--- tweets.rb
+++ tweets.rb
@@ -1,8 +1,10 @@
+require_relative 'update'
+
 class Tweets
     def latest
-        update = Update.new
-        update.reaction = 'Sad'
-        update.url = 'http://uk.reuters.com/article/2012/02/06/markets-global-idUKL2E8D6GUG20120206'
-        update.reason = 'everyone is waiting for greece to do something'
+        @update = Update.new
+        @update.reaction = 'Sad'
+        @update.url = 'http://uk.reuters.com/article/2012/02/06/markets-global-idUKL2E8D6GUG20120206'
+        @update.reason = 'everyone is waiting for greece to do something'
     end    
 end
--- update.rb
+++ update.rb
@@ -1,3 +1,18 @@
 class Update
     attr_accessor :reaction, :reason, :url
+
+    
+    def initialize(tweet)
+        @reactions = ['sad', 'happy']    
+        @reaction = tweet.split[0].chomp
+        @url = tweet.split[1].chomp
+        @reason = tweet.split[2..tweet.split.length].join(" ")
+        validate
+    end
+
+    def validate
+        if not @reactions.include?(@reaction.downcase)
+            raise ArgumentError, "Invalid reaction"
+        end
+    end
 end
