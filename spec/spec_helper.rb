f8b233aaf760b4455b11907d7a993334f82d07bf
Ged <macdermott@gmail.com>
2012-02-17 00:18:19 +0000

    Added helper to hold tweet constants

---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+VALID_TWEET = "Sad http://goo.gl/D07Lw Standard & Poor are threatening downgrades in Europe"
+INVALID_REACTION_TWEET = "Thoughtative http://goo.gl/D07Lw Standard & Poor are threatening downgrades in Europe"
+INVALID_URL_TWEET = "Sad htt:/goo.gl/ASDF Sad Markets"
